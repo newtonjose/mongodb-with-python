0e61f8ad3b4ac305d137ee5ab31373effdd04ffb
Newton José <josephnewton001@gmail.com>
2025-05-02 19:30:39 -0300

    feat: mongo connecting (#1)
    * feat: mongo connecting
    
    * feat: add ruff linter
    
    * chore: remove old db connectin
    
    * chore: add logger info

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mongodb-with-python
 Using MongoDB with Python by 'MongoDB Developer Data Platform' at In Learning
+
+# Local Setup
+```sh
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+### .env.example
+```
+cp .env.example app/.env
+```
+
+# Run
+```sh
+python -m app.database
+```
---
+++ app/database.py
@@ -0,0 +1,24 @@
+from pymongo import errors, MongoClient
+
+from .settings import settings
+from .utils import get_logger
+
+logger = get_logger(__name__)
+
+try:
+    client = MongoClient(settings.mongodb_uri)
+
+    for db_name in client.list_database_names():
+        logger.info(db_name)
+
+    client.close()
+except errors.InvalidURI as e:
+    logger.warning(e)
+except errors.ConfigurationError as e:
+    logger.warning(e)
+except errors.OperationFailure as e:
+    logger.error(e)
+except errors.ServerSelectionTimeoutError:
+    logger.error(
+        "Cluster not reachable. Please check your network connection and IP whitelisting"
+    )
